39f16cb0e2c4aa43b09d9b9542a1768da3b843ce
Zech Zimmerman <hi@zech.codes>
2021-07-22 17:37:37 -0400

    Disable the dev secret key in production

--- website/zechcodes/zechcodes/settings/__init__.py
+++ website/zechcodes/zechcodes/settings/__init__.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dqpiv=6n2go^55h33e$c4u^uviyk2yvi=bsauk)&%i%*haiinx"
+# SECRET_KEY = "django-insecure-dqpiv=6n2go^55h33e$c4u^uviyk2yvi=bsauk)&%i%*haiinx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
