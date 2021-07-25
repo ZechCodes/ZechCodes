cc038c6c033a7d58c0f03155ae11790bfc276cc0
Zech Zimmerman <hi@zech.codes>
2021-07-25 14:11:11 -0400

    Remove website, using Hashnode

--- website/zechcodes/home_page/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from home_page.views import home_page
-
-
-urlpatterns = [
-    url(r"^$", home_page, name="homepage"),
-]
--- website/zechcodes/home_page/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.conf import settings
-
-
-def home_page(request):
-    context = {"google_analytics_key": settings.GOOGLE_ANALYTICS_KEY}
-    return render(request, "home_page.html", context)
